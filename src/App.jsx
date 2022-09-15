05243ad136c13b3342f3a63e063651c0d96c24db
Fatih Kabay <mfthkabay@gmail.com>
2022-09-15 23:55:26 +0300

    import Europa.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import './App.css';
-import { Routes, Route, Link } from "react-router-dom"
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Europa from "./Europa";
+
 function App() {
   return (
     <div className="container">
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Europa" element={<Europa />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="Home-page">
+      <h1>Welcome Home Page</h1>
+      <Link className="Link" to="/Europa">
+        Europa
+      </Link>
     </div>
   );
 }
---
+++ src/Europa.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+function Europa() {
+  return (
+    <div className="Europa-page">
+      <h1>Welcome Europa Page</h1>
+      <p>
+        The area of Europa is approximately 10.18 million square kilometers,
+        making it the smallest makes the second continent. Geographically, it
+        accounts for 39% of the continent's population.
+        <a target="blank" href="https://tr.wikipedia.org/wiki/Rusya">
+          Russia
+        </a>
+        is Europe's largest and most populous country, accounting for 15%. is
+        his country. As of 2021, the total population of Europe is approximately
+        745 is million.The currency of most European Union countries is the
+        <a target="blank" href="https://tr.wikipedia.org/wiki/Euro">
+          Euro
+        </a>
+        .
+      </p>
+      <Link className="link" to="/">
+        Home
+      </Link>
+    </div>
+  );
+}
+export default Europa;
