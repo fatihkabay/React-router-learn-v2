32e7f13ae1d5c9d5c75d471e55fde7d12f93db13
Fatih Kabay <mfthkabay@gmail.com>
2022-09-16 23:34:30 +0300

    error fixed and beautiy file

--- src/continents/Africa.jsx
+++ src/continents/Africa.jsx
@@ -1,20 +1,20 @@
-import Link from "react-router-dom"
+import { Link } from "react-router-dom";
 
 function Africa() {
-    return (
-      <div className="page">
-        <h1>Welcome Africa Page</h1>
-        <p>
-          Africa is the world's second largest continent in terms of area and
-          population density. It covers 6% of the world's surface area and 24.4%
-          of the world's land area, with an area of 30.8 million square
-          kilometers, together with the islands considered adjacent to it.
-        </p>
-        <Link className="link" to="/">
-          Home
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div className="page">
+      <h1>Welcome Africa Page</h1>
+      <p>
+        Africa is the world's second largest continent in terms of area and
+        population density. It covers 6% of the world's surface area and 24.4%
+        of the world's land area, with an area of 30.8 million square
+        kilometers, together with the islands considered adjacent to it.
+      </p>
+      <Link className="link" to="/">
+        Home
+      </Link>
+    </div>
+  );
+}
 
-  export default Africa
+export default Africa;
